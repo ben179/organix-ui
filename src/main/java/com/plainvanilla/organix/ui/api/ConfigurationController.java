10c3d8afbc36cc66f42e03141c9504924707a4e1
ben179 <bencikmartin@seznam.cz>
2014-01-05 13:37:45 +0100

    Patrial commit

--- src/main/java/com/plainvanilla/organix/ui/api/ConfigurationController.java
+++ src/main/java/com/plainvanilla/organix/ui/api/ConfigurationController.java
@@ -7,8 +7,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
@@ -19,8 +22,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.plainvanilla.organix.engine.model.Configuration;
 import com.plainvanilla.organix.engine.model.ConnectionType;
@@ -30,10 +36,11 @@ import com.plainvanilla.organix.engine.services.ConfigurationService;
 
 
 @Controller
-@RequestMapping("/configuration")
+@RequestMapping(value="/configuration", produces = {"application/json"}, consumes = {"application/x-www-form-urlencoded"})
 public class ConfigurationController {
 
 	private MappingJacksonJsonView configurationView;
+	private ObjectMapper configurationObjectMapper;
 	private ConfigurationService configurationService;
 	
 	@Autowired
@@ -41,12 +48,18 @@ public class ConfigurationController {
 		this.configurationView = configurationView;
 	}
 
+	
+	@Autowired
+	public void setConfigurationObjectMapper(ObjectMapper configurationObjectMapper) {
+		this.configurationObjectMapper = configurationObjectMapper;
+	}
+
 	@Autowired
 	public void setConfigurationService(ConfigurationService configurationService) {
 		this.configurationService = configurationService;
 	}
 
-	@RequestMapping(method=RequestMethod.GET, produces = {"application/json"}, consumes = {"application/x-www-form-urlencoded"})
+	@RequestMapping(method=RequestMethod.GET/*, produces = {"application/json"}, consumes = {"application/x-www-form-urlencoded"}*/)
 	public View getAllConfigurations(@RequestParam(required=false) boolean headersOnly, Model model) {
 		List<Configuration> configurations = configurationService.getAllConfigurations(headersOnly);
 		model.addAttribute(configurations);
@@ -66,7 +79,7 @@ public class ConfigurationController {
 	}
 	
 	
-	@RequestMapping(value="/{configId}/objectType", method=RequestMethod.POST, produces = {"application/json"}, consumes = {"application/json"})
+	@RequestMapping(value="/{configId}/objectType", method=RequestMethod.POST, /*produces = {"application/json"},*/ consumes = {"application/json"})
 	@ResponseStatus(HttpStatus.CREATED)
 	public View createObjectType(@PathVariable Long configId, @RequestBody @Validated ObjectType type, Model model) {
 		ObjectType t = configurationService.addObjectType(type.getTypeNumber(), type.getName(), configId);
@@ -83,7 +96,7 @@ public class ConfigurationController {
 	}	
 	
 	
-	@RequestMapping(value="/{configId}", method=RequestMethod.GET, produces = {"application/json"}, consumes = {"application/x-www-form-urlencoded"})
+	@RequestMapping(value="/{configId}", method=RequestMethod.GET /*, produces = {"application/json"}, consumes = {"application/x-www-form-urlencoded"}*/)
 	public View getConfiguration(@PathVariable Long configId, Model model) {
 		Configuration config = configurationService.getConfiguration(configId);
 		model.addAttribute(config);
@@ -101,23 +114,35 @@ public class ConfigurationController {
 		configurationView.getObjectMapper().writeValue(response.getOutputStream(), config);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, produces = {"application/json"}, consumes = {"application/x-www-form-urlencoded"})
+	@RequestMapping(method=RequestMethod.POST/*, produces = {"application/json"}, consumes = {"application/x-www-form-urlencoded"}*/)
 	@ResponseStatus(HttpStatus.CREATED)
-	public View createConfiguration(@RequestParam String name, Model model) {
+	public View createConfiguration(@RequestParam String name, Model model, HttpServletResponse response) {
 		Configuration config = configurationService.createNewConfiguration(name);
 		model.addAttribute(config);
 		return configurationView;
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, consumes = {"application/json"})
-	@ResponseStatus(HttpStatus.CREATED)
-	public View uploadConfiguration(@RequestBody @Validated Configuration config, Model model) {
-		Configuration uploaded = configurationService.importConfiguration(config);
-		model.addAttribute(uploaded);
-		return configurationView;
+	@RequestMapping(value="/upload", method=RequestMethod.POST/*, produces = {"application/json"}*/, consumes = {"multipart/form-data"})
+	//@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Configuration> importConfiguration(@RequestParam("ConfigFile") MultipartFile file/*, Model model*/, UriComponentsBuilder b) throws IOException {
+		Configuration config = configurationObjectMapper.readValue(file.getBytes(), Configuration.class);
+		return uploadConfiguration(config/*, model*/, b);
 	}
 	
-	@RequestMapping(value="/{configId}/database", method=RequestMethod.POST, produces = {"application/json"}, consumes = {"application/x-www-form-urlencoded"})
+	@RequestMapping(method=RequestMethod.POST, consumes = {"application/json"})
+//	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Configuration> uploadConfiguration(@RequestBody @Validated Configuration config/*, Model model*/, UriComponentsBuilder b) {
+		Configuration uploaded = configurationService.importConfiguration(config);
+		//model.addAttribute(uploaded);
+
+		UriComponents c = b.path("/configuration/{id}").buildAndExpand(uploaded.getId());
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(c.toUri());
+		return new ResponseEntity<Configuration>(uploaded, headers, HttpStatus.CREATED);
+		//return configurationView;
+	}
+	
+	@RequestMapping(value="/{configId}/database", method=RequestMethod.POST/*, produces = {"application/json"}, consumes = {"application/x-www-form-urlencoded"}*/)
 	@ResponseStatus(HttpStatus.CREATED)
 	public View createDatabase(@RequestParam String dbName, @PathVariable Long configId, Model model) {
 		Database db = configurationService.createNewDatabase(dbName, configId); 
@@ -125,7 +150,7 @@ public class ConfigurationController {
 		return configurationView;
 	}
 	
-	@RequestMapping(value="/{configId}/connectionType", method=RequestMethod.POST, produces = {"application/json"}, consumes = {"application/json"})
+	@RequestMapping(value="/{configId}/connectionType", method=RequestMethod.POST/*, produces = {"application/json"}*/, consumes = {"application/json"})
 	@ResponseStatus(HttpStatus.CREATED)
 	public View createConnectionType(@PathVariable Long configId, @RequestBody @Validated ConnectionType type, Model model) {
 		model.addAttribute(configurationService.addConnectionType(type.getTypeNumber(), type.getSourceEnd().getRoleName(), type.getSourceEnd().getObjectType(), type.getSourceEnd().getUnique(), type.getSourceEnd().getMandatory(), type.getTargetEnd().getRoleName(), type.getTargetEnd().getObjectType(), type.getTargetEnd().getUnique(), type.getTargetEnd().getMandatory(), configId));
--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -2,6 +2,113 @@ var organixControllers = angular.module('organixControllers', []);
 
 organixControllers.controller('ConfigurationCtrl', function($scope, $http) {
 	
+
+
+	
+	   //============== DRAG & DROP =============
+    // source for drag&drop: http://www.webappers.com/2011/09/28/drag-drop-file-upload-with-html5-javascript/
+    var dropbox = document.getElementById("dropbox");
+    $scope.dropText = 'Drop files here...';
+
+    // init event handlers
+    function dragEnterLeave(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        $scope.$apply(function(){
+            $scope.dropText = 'Drop files here...';
+            $scope.dropClass = '';
+        });
+    }
+    dropbox.addEventListener("dragenter", dragEnterLeave, false);
+    dropbox.addEventListener("dragleave", dragEnterLeave, false);
+    dropbox.addEventListener("dragover", function(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        var clazz = 'not-available';
+        var ok = evt.dataTransfer && evt.dataTransfer.types && evt.dataTransfer.types.indexOf('Files') >= 0;
+        $scope.$apply(function(){
+            $scope.dropText = ok ? 'Drop files here...' : 'Only files are allowed!';
+            $scope.dropClass = ok ? 'over' : 'not-available';
+        });
+    }, false);
+    dropbox.addEventListener("drop", function(evt) {
+        console.log('drop evt:', JSON.parse(JSON.stringify(evt.dataTransfer)));
+        evt.stopPropagation();
+        evt.preventDefault();
+        $scope.$apply(function(){
+            $scope.dropText = 'Drop files here...';
+            $scope.dropClass = '';
+        });
+        var files = evt.dataTransfer.files;
+        if (files.length > 0) {
+            $scope.$apply(function(){
+                $scope.files = [];
+                for (var i = 0; i < files.length; i++) {
+                    $scope.files.push(files[i]);
+                }
+            });
+        }
+    }, false);
+    //============== DRAG & DROP =============
+
+    $scope.setFiles = function(element) {
+    $scope.$apply(function($scope) {
+      console.log('files:', element.files);
+      // Turn the FileList object into an Array
+        $scope.files = [];
+        for (var i = 0; i < element.files.length; i++) {
+          $scope.files.push(element.files[i]);
+        }
+      $scope.progressVisible = false;
+      });
+    };
+
+    $scope.uploadFile = function() {
+        var fd = new FormData();
+        for (var i in $scope.files) {
+            fd.append("ConfigFile", $scope.files[i]);
+        }
+        var xhr = new XMLHttpRequest();
+        xhr.upload.addEventListener("progress", uploadProgress, false);
+        xhr.addEventListener("load", uploadComplete, false);
+        xhr.addEventListener("error", uploadFailed, false);
+        xhr.addEventListener("abort", uploadCanceled, false);
+        xhr.open("POST", "/organix/configuration/upload");
+        $scope.progressVisible = true;
+        xhr.send(fd);
+    };
+
+    function uploadProgress(evt) {
+        $scope.$apply(function(){
+            if (evt.lengthComputable) {
+                $scope.progress = Math.round(evt.loaded * 100 / evt.total);
+            } else {
+                $scope.progress = 'unable to compute';
+            }
+        });
+    };
+
+    function uploadComplete(evt) {
+        /* This event is raised when the server send back a response */
+        alert(evt.target.responseText);
+        
+		$scope.configuration = evt.target.responseText;
+		$scope.configurationUrl = '/organix/configuration/' + $scope.configuration.id;
+		$scope.loadConfigHeaders();
+		$scope.currentConfigurationHeader = evt.target.responseText.configuration.id;
+    };
+
+    function uploadFailed(evt) {
+        alert("There was an error attempting to upload the file.");
+    };
+
+    function uploadCanceled(evt) {
+        $scope.$apply(function(){
+            $scope.progressVisible = false;
+        });
+        alert("The upload has been canceled by the user or the browser dropped the connection.");
+    };
+	
 	$scope.newConfiguration = {};
 	$scope.newConfiguration.version = 1;
 	$scope.configurationsHeaders = null;
@@ -13,8 +120,7 @@ organixControllers.controller('ConfigurationCtrl', function($scope, $http) {
 	$scope.reversePropObject = false;
 	$scope.orderPropConnection = 'typeNumber';
 	$scope.reversePropConnection = false;
-
-	
+    
 	$scope.loadConfigHeaders = function() {
 		
 		$http({
@@ -22,7 +128,7 @@ organixControllers.controller('ConfigurationCtrl', function($scope, $http) {
 			url: '/organix/configuration',
 			data: 'headersOnly=true',
 			headers: {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'} 
-		}).success(function(data) {
+		}).success(function(data, status, headers, config) {
 			$scope.configurationHeaders = data.configurationList;
 			$scope.configurationsExist = data.configurationList.length > 0;
 			$scope.newConfiguration = {};			
@@ -38,11 +144,11 @@ organixControllers.controller('ConfigurationCtrl', function($scope, $http) {
 			url: '/organix/configuration',
 			data: angular.toJson(configuration),
 			headers: {'Content-Type': 'application/json'}
-		}).success(function(data){
-			$scope.configuration = data.configuration;
-			$scope.configurationUrl = '/organix/configuration/' + $scope.configuration.id;
+		}).success(function(data, status, headers, config){
+			$scope.configuration = data;
+			$scope.configurationUrl = headers('Location');
 			$scope.loadConfigHeaders();
-			$scope.currentConfigurationHeader = data.configuration.id;
+			$scope.currentConfigurationHeader = data.id;
 		});
 				
 	};
@@ -131,12 +237,19 @@ organixControllers.controller('ConfigurationCtrl', function($scope, $http) {
 			url: '/organix/configuration/' + $scope.currentConfigurationHeader,
 			data: 'headersOnly=true',
 			headers: {'Content-Type':'application/x-www-form-urlencoded'} 
-		}).success(function(data) {
-			$scope.configuration = data.configuration;
-			$scope.configurationUrl = '/organix/configuration/' + data.configuration.id;
+		}).success(function(data, status, headers, config) {
+			$scope.configuration = data;
+			$scope.configurationUrl = headers('Location');
 		});
 	};
 
+	$scope.importConfigFile = function(fileName) {
+		 $http.get(fileName)
+	       .then(function(res){
+	    	   $scope.newConfiguration = res;                
+	        });
+		
+	};
 	
 	$scope.loadConfigHeaders();
 	
